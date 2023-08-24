5011caf3ee834063d5d1fa214e6ec5b50a5f734b
Terrance Valdez <terrancejvaldez@gmail.com>
2023-08-23 21:47:02 -0700

    Add project files.

---
+++ ModernVPN/MVVM/ViewModel/ProtectionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernVPN.MVVM.ViewModel
+{
+    internal class ProtectionViewModel
+    {
+        public ObservableCollection<ServerModel> MyProperty { get; set; }
+        public ProtectionViewModel()
+        {
+            
+        }
+    }
+}
